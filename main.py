94c0979929d807e17a99addd58c757b832058963
Johny Lam <johny@pocketgems.com>
2019-10-31 15:46:18 -0700

    connect google cloud logging

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from app import application
 from werkzeug.middleware.proxy_fix import ProxyFix
+import google.cloud.logging
 
 application.wsgi_app = ProxyFix(application.wsgi_app, x_proto=1)
+
+client = google.cloud.logging.Client()
+client.setup_logging()
 
 if __name__ == "__main__":
     application.run()
